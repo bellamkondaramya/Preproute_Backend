5e1a89ad0d9c0dc0c428b0884141b54a563d0d6d
Bellamkonda Ramya <bellamkondaramya12345@gmail.com>
2026-06-11 19:30:07 +0530

    .env

--- src/config/env.js
+++ src/config/env.js
@@ -8,5 +8,5 @@ export const env = {
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://Preproute:ramya123@cluster0.fxxkf4u.mongodb.net/preproute_test_management?retryWrites=true&w=majority&appName=Cluster0',
   JWT_SECRET: process.env.JWT_SECRET || 'production-super-secret-jwt-key-change-this',
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '7d',
-  CORS_ORIGIN: process.env.CORS_ORIGIN || 'http://localhost:5173,http://localhost:3000,https://r2f.vercel.app,https://r2-lp2wcm0tg-simhadrimalladis-projects.vercel.app,https://preproute-frontend-drab.vercel.app'
+  CORS_ORIGIN: process.env.CORS_ORIGIN || 'http://localhost:5173,http://localhost:3000,https://r2f.vercel.app,https://r2-lp2wcm0tg-simhadrimalladis-projects.vercel.app,https://preproute-frontend-drab.vercel.app,https://preproute-frontend-i72qzap2w-bellamkondaramyas-projects.vercel.app'
 };
